234d2ca2038fbb95e9e2e22a53b66df07dc0cef6
Faizalfmi <faizalfahmi490@gmail.com>
2023-04-05 08:26:26 +0700

    Revert "Revert "add controller""
    This reverts commit 9417ade1ac1f9df83b6b7481a1c48cfd7aa502f7.

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_property' => 'required',
+            'id_user' => 'required',
+            'tanggal_sewa' => 'required',
+            'status_pembayaran' => 'required'
+            
+        ]);
+
+        Reservation::create([
+            'id_property' => $request->id_property,
+            'id_user' => $request->id_user,
+            'tanggal_sewa' => $request->tanggal_sewa,
+            'status_pembayaran' => $request->status_pembayaran,
+        ]);
+
+
+        return redirect('/history')->with('success', 'Property ditambahkan ke favorit');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'id_property' => 'required',
+            'id_user' => 'required',
+            'tanggal_sewa' => 'required',
+            'status_pembayaran' => 'required'
+        ]);
+
+
+        $property = Reservation::find($id);
+        
+        $property->update([
+            'id_property' => $request->id_property,
+            'id_user' => $request->id_user,
+            'tanggal_sewa' => $request->tanggal_sewa,
+            'status_pembayaran' => $request->status_pembayaran,
+        ]);
+        return redirect('/product')->with('success', 'Property berhasil diupdate');
+    }
+
+    public function destroy($id)
+    {
+        $property = Reservation::find($id);
+        
+        $property->delete();
+        return redirect('/product')->with('success', 'Property berhasil dihapus');
+    }
 }
