d1c9835552252681b37370eece5a5636eed070ca
Faizalfmi <faizalfahmi490@gmail.com>
2023-04-10 21:01:32 +0700

    pengajuan sewa resolve #5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +18,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/registrasi', function () {
+    return view('registrasi');
+});
+
+Route::get('/add_property', function () {
+    return view('addProperty');
+});
+
+Route::get('/property', function () {
+    $data = DB::table('properties')->get();
+    return view('property', ['data' => $data]);
+});
+
+Route::get('/detail/{id}', function ($id) {
+    $data = DB::table('properties')->where('id', $id)->first();
+    return view('detail', ['data' => $data]);
+});
+
+Route::get('/my_property/{id}', function ($id) {
+    $data = DB::table('properties')->where('id_pemilik', $id)->first();
+    return view('myProperty', ['data' => $data]);
+});
+
+Route::get('/reservasi/{id}', function ($id) {
+    $data = DB::table('properties')->where('id', $id)->first();
+    return view('reservasi', ['data' => $data]);
+});
+
+
+
+
+
+Route::post('/register', [UserController::class, 'store']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/logout', [UserController::class, 'logout']);
+Route::get('/search', 'App\Http\Controllers\SearchController@search')->name('search');
+Route::post('/add_property', [PropertyController::class, 'store']);
