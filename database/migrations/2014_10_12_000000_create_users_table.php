f66b935f8cffe56a31e40fa1da238a5e26d8df87
Faizalfmi <faizalfahmi490@gmail.com>
2023-04-11 14:38:16 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id')->primary();
+            $table->bigIncrements('id');
             $table->string('nama');
             $table->string('no_hp');
             $table->string('alamat');
